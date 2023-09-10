2857eceb0352ed3b3d6395ea291e627ee41b96ac
Rajaa C <96713906+RC2110@users.noreply.github.com>
2023-09-10 11:27:11 +0530

    Added an edit case in program. Similar live list box updation using update function.

--- gui.py
+++ gui.py
@@ -43,9 +43,9 @@ import PySimpleGUI as psg
 import functions
 
 label1= psg.Text("Enter a Todo", font=('Helvetica', 15))
-userip=psg.InputText(tooltip="Enter Todo", font=('Helvetica', 15), size=(33,10),
+userip=psg.InputText(tooltip="Enter Todo", font=('Helvetica', 15), size=[33,10],
                      key="usr_ip")
-e_lst=psg.Listbox(functions.get_todos(), enable_events=True, size=(50, 10), key='elist')
+e_lst=psg.Listbox(functions.get_todos(), enable_events=True, size=[50, 10], key='elist')
 elst_button=psg.Button("Edit")
 button=psg.Button("Add")
 
@@ -54,9 +54,9 @@ window = psg.Window("Your Todo app", layout=[[label1],[userip,button],[e_lst,els
 while True:
     event = window.read()
     action, input = event
-    print(event)
-    print(action)
-    print(input)
+    print(1,"event", event)
+    print(2,"action",action)
+    print(3,"input",input)
     match action:
         case "Add":
             e_todos= functions.get_todos()
@@ -65,13 +65,18 @@ while True:
             functions.write_todos(e_todos)
             window['elist'].update(values=e_todos)
             print(e_todos)
-
+        case "Edit":
+            todos=functions.get_todos()
+            ntodo=input['usr_ip']
+            tdo2edit=input['elist'][0]
+            indx=todos.index(tdo2edit)
+            todos[indx]=ntodo
+            functions.write_todos(todos)
+            window['elist'].update(values=todos)
+        case "elist":
+            window['usr_ip'].update(value=input['elist'][0])
         case psg.WIN_CLOSED:
             break
-
-
-
-
 
 window.close()
 
