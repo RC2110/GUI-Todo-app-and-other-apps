c48f46879004573328137b4fd79e21aa9858d733
Rajaa C <96713906+RC2110@users.noreply.github.com>
2023-09-18 08:31:49 +0530

    build a web app using Streamlit-with Complete function.

--- webapp.py
+++ webapp.py
@@ -1,12 +1,26 @@
 import streamlit as st
 import functions
-todos=functions.get_todos()
-st.title("Your Todo App")
-st.header("Welcome!")
-st.subheader("Make your Day Productive!")
-st.write("Get started!!")
 
-for i in todos:
-    st.checkbox(i)
+todos = functions.get_todos()
+def addtodo():
+    tdo=st.session_state['newtodo']+'\n'
+    todos.append(tdo)
+    functions.write_todos(todos)
 
-st.text_input(label="",placeholder="Type in a Todo")
+st.title("Your Todos App")
+st.header("Welcome! Get your day more productive!")
+st.subheader("Get Started!")
+
+
+for index, i in enumerate(todos):
+    check = st.checkbox(i,key=i)
+    print(check)
+    if check:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[i]
+        st.experimental_rerun()
+
+st.text_input(label='',placeholder="Type in a To-Do", on_change=addtodo, key='newtodo')
+st.session_state
+
